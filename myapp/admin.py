69088dcc4e9f69e80589e3f64f75e646fb048802
MGMaikeru <13miguegonza2005@gmail.com>
2024-03-10 18:46:07 -0500

    Delete credentials model

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import academy_Users_Credentials
+from .models import EventRequest
 
 # Register your models here.
-admin.site.register(academy_Users_Credentials)
+admin.site.register(EventRequest)
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
     
 urlpatterns = [
-    path('academicUsersLogin/', views.academicUsersLogin, name= 'academicUsersLogin'),
+    path('academic_users_login/', views.academicUsersLogin, name= 'academicUsersLogin'),
     path('index/', views.index, name='index'),
     path('logout/', views.signout, name= 'logout'),
     path('create-event-request/', views.create_event_request, name='create_event_request'),
