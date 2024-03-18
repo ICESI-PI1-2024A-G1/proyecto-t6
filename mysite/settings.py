6cb30a01cc092caf44dda6dcba43603d67be73c3
MGMaikeru <13miguegonza2005@gmail.com>
2024-03-18 18:37:09 -0500

    Upload bootstrap librery

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'america/bogota'
 
 USE_I18N = True
 
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
