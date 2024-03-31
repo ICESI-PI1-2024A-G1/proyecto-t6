5ab283e001ea596ed508fb12ed036f7a8083daa4
Santiago Castillo <sacalla3@gmail.com>
2024-03-31 02:02:11 -0500

    test: test_models EventRequestTestCase

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,17 @@
+import unittest
 from django.test import TestCase
+from .forms import EventRequestForm, EstadoSolicitudForm
 
-# Create your tests here.
+class TestEventRequestForm(TestCase):
+    def test_valid_data(self):
+        form_data = {
+            'lugar': 'Sala de conferencias',
+            'fecha_inicio': '2024-04-01',
+            'fecha_fin': '2024-04-03',
+            'presupuesto': 1000,
+            'alimentacion': True,
+            'transporte': False,
+            'profesor': 'Juan Pérez'
+        }
+        form = EventRequestForm(data=form_data)
+        self.assertTrue(form.is_valid())
