e7fb9f6dfb6f0bbb41b5ba24a584f81b0150d782
Santiago Castillo <sacalla3@gmail.com>
2024-04-14 05:26:44 -0500

    feat: added functional notification box

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,7 +72,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+                'myapp.context_processors.notificaciones',
+            ]
         },
     },
 ]
