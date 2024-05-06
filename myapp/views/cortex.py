69d37b8261cf62151320a3beafad8413fb7cf222
MGMaikeru <13miguegonza2005@gmail.com>
2024-05-05 19:00:23 -0500

    fix: calendar events render

--- myapp/views/cortex.py
+++ myapp/views/cortex.py
@@ -14,26 +14,8 @@ class cortex(ListView):
     def index(request):
         group = request.user.groups.values_list("id", flat=True).first()
         firstName = None
-        if request.user.is_authenticated:
-            firstName = request.user.first_name
 
-        if group == 1:
-            template_name = "index1.html"
-            return render(request, "index1.html", {'first_name': firstName})
-        if group == 2:
-            return render(request, "index2.html", {'first_name': firstName})
-
-        return render(request, "index3.html", {'first_name': firstName})
-
-    def get_queryset(self):
-        """
-        Get the classes associated with a given teacher ID.
-
-        Returns:
-        list: List of dictionaries containing class information.
-        """
-        # Get the classes associated with a given teacher
-        queryset = self.model.objects.filter(estado_solicitud="En curso").all()
+        queryset = Event.objects.filter(estado_solicitud="En curso").all()
         print(queryset)
         # Initialize the array of out
         out = []
@@ -44,4 +26,16 @@ class cortex(ListView):
                 'start': event.fecha_inicio.strftime('%Y-%m-%d'),
                 'end': event.fecha_fin.strftime('%Y-%m-%d')
             })
-        return out
+        
+
+        if request.user.is_authenticated:
+            firstName = request.user.first_name
+
+        if group == 1:
+            template_name = "index1.html"
+            return render(request, "index1.html", {'first_name': firstName, 'object_list':out})
+        if group == 2:
+            return render(request, "index2.html", {'first_name': firstName, 'object_list':out})
+
+        return render(request, "index3.html", {'first_name': firstName})
+
